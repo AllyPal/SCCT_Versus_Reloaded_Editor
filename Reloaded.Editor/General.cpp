3d68f4682a241ba215e857b03f0fc6d1d36fb534
Jon <jt-ps@outlook.com>
2026-05-16 20:53:18 +0100

    use mimalloc

--- Reloaded.Editor/General.cpp
+++ Reloaded.Editor/General.cpp
@@ -1,7 +1,8 @@
 #include "pch.h"
 #include "General.h"
-
 #include "Hooks.h"
+#include "MemoryWriter.h"
+#include <mimalloc.h>
 
 INIT_HOOKS;
 
@@ -12,7 +13,32 @@ JMP_HOOK(0x110518AD, RemoveAudioSizeLimit) {
     }
 }
 
+// Memory redirection addresses to be provided by the user
+namespace MemoryAddresses {
+    uintptr_t malloc_addr = 0x11AF2114;
+    uintptr_t free_addr = 0x11AF209C;
+    uintptr_t realloc_addr = 0x11AF21F0;
+    uintptr_t calloc_addr = 0x11AF2098;
+    uintptr_t strdup_addr = 0x11AF21C0;
+}
+
+void InstallMemoryHooks() {
+    uintptr_t fn_ptr;
+
+    fn_ptr = reinterpret_cast<uintptr_t>(mi_malloc);
+    MemoryWriter::WriteBytes(MemoryAddresses::malloc_addr, &fn_ptr, sizeof(fn_ptr));
+    fn_ptr = reinterpret_cast<uintptr_t>(mi_free);
+    MemoryWriter::WriteBytes(MemoryAddresses::free_addr, &fn_ptr, sizeof(fn_ptr));
+    fn_ptr = reinterpret_cast<uintptr_t>(mi_realloc);
+    MemoryWriter::WriteBytes(MemoryAddresses::realloc_addr, &fn_ptr, sizeof(fn_ptr));
+    fn_ptr = reinterpret_cast<uintptr_t>(mi_calloc);
+    MemoryWriter::WriteBytes(MemoryAddresses::calloc_addr, &fn_ptr, sizeof(fn_ptr));
+    fn_ptr = reinterpret_cast<uintptr_t>(mi_strdup);
+    MemoryWriter::WriteBytes(MemoryAddresses::strdup_addr, &fn_ptr, sizeof(fn_ptr));
+}
+
 void General::Initialize()
 {
     INSTALL_HOOKS;
+    InstallMemoryHooks();
 }
